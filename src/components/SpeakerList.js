59f06ed664f5caf30e1bc9d12b0575033ef1cf8b
pkellner <peter@peterkellner.net>
2020-09-08 11:39:38 -0700

    wipwiopwipwip

--- src/components/SpeakerList.js
+++ src/components/SpeakerList.js
@@ -24,19 +24,24 @@ function SpeakerList() {
     return (
         <div className="container">
             {speakers.map(({ id, name, company, sessions, cursor }) => {
+                //const speakerSrc = `https://www.siliconvalley-codecamp.com/attendeeimage/32604.jpg?height=100`;
+                const speakerSrc = `http://ddrt7tzfkdwdf.cloudfront.net/attendeeimage/${id}.jpg?height=40`;
                 return (
                     <div className="row justify-content-start" key={id}>
-                        <b>{name} </b>&nbsp;&nbsp;{company}
+                        <b>{name} </b>&nbsp;&nbsp;{company} &nbsp;&nbsp;{' '}
+                        <img src={speakerSrc} alt={name} />
                         <div className="container">
-                            {sessionsBySpeaker(id).slice(0,3).map((session) => {
-                                return (
-                                    <div className="row" key={session.id}>
-                                        &nbsp;&nbsp;{session.title}{' '}
-                                        &nbsp;&nbsp;&nbsp;Year:
-                                        {session.eventYear}
-                                    </div>
-                                );
-                            })}
+                            {sessionsBySpeaker(id)
+                                .slice(0, 3)
+                                .map((session) => {
+                                    return (
+                                        <div className="row" key={session.id}>
+                                            &nbsp;&nbsp;{session.title}{' '}
+                                            &nbsp;&nbsp;&nbsp;Year:
+                                            {session.eventYear}
+                                        </div>
+                                    );
+                                })}
                         </div>
                     </div>
                 );
